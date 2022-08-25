8826107cf79b6b7ec7245138df1f2b66ea303032
Mabel Betancur <m.betancurtm@gmail.com>
2022-08-24 23:30:23 -0500

    Parameters are added to a list but it is not reversed

---
+++ reverse_list.py
@@ -0,0 +1,17 @@
+"""def reverse_list(*elements):
+    lists = []
+    for elements in lists:
+        yus = lists.append(elements)
+        lists.reverse(yus)
+    return lists.reverse(elements)
+print(reverse_list(1, 3, 5, 4))"""
+
+def reverse_lists(*elements):
+    fruits = []
+    print(fruits)
+    fruits1 = fruits.append(elements)
+    print(fruits)
+    fruits1.reverse(fruits)
+    #fruits.reverse()
+    return fruits1
+reverse_lists("banana", "orange", "mango", "lemon")

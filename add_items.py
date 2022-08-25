6c7ef7c895a71536c5eb76cfda77dcff9531802c
Mabel Betancur <m.betancurtm@gmail.com>
2022-08-25 16:30:34 -0500

    Its possible to get better and returns just one list

---
+++ add_items.py
@@ -0,0 +1,8 @@
+def add_item(total_university, another_university):
+    universities = []
+    universities.append(total_university)
+    universities.append(another_university)
+    return universities
+print(add_item(["Externado", "Andes", "Rosario"], "Javeriana"))
+
+#Its possible to get better and returns just one list

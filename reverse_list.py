40717942805a2ffc87fa1df0c4b3601987355637
Mabel Betancur <m.betancurtm@gmail.com>
2022-08-24 23:53:44 -0500

    Try to solve the problem since zero

--- reverse_list.py
+++ reverse_list.py
@@ -1,17 +1,16 @@
-"""def reverse_list(*elements):
-    lists = []
-    for elements in lists:
-        yus = lists.append(elements)
-        lists.reverse(yus)
-    return lists.reverse(elements)
-print(reverse_list(1, 3, 5, 4))"""
+# def reverse_lists(*elements):
+#     fruits = []
+#     fruits.append(elements)
+#     print(fruits)
+#     for element in fruits:
+#         print(element)
+#         print(fruits)
+#         print(elements)
+#         print(fruits.reverse())
+#         reversedl = fruits.reverse()
+#     return reversedl
+# reverse_lists("banana", "orange", "mango", "lemon")
 
-def reverse_lists(*elements):
-    fruits = []
-    print(fruits)
-    fruits1 = fruits.append(elements)
-    print(fruits)
-    fruits1.reverse(fruits)
-    #fruits.reverse()
-    return fruits1
-reverse_lists("banana", "orange", "mango", "lemon")
+fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits.reverse()
+print(fruits)

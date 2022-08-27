8f6a7bb6c941f013993759a3f094afbb26fcc69c
Mabel Betancur <m.betancurtm@gmail.com>
2022-08-26 22:50:08 -0500

    Corrección

--- add_items.py
+++ add_items.py
@@ -2,6 +2,8 @@ def add_item(total_university, another_university):
     universities = []
     universities.append(total_university)
     universities.append(another_university)
+    definetly_list = total_university + another_university
+    print(definetly_list)
     return universities
 print(add_item(["Externado", "Andes", "Rosario"], "Javeriana"))
 

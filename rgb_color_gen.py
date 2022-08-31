ab74c447c02494d9733d55225e410514b5295a34
Mabel Betancur <m.betancurtm@gmail.com>
2022-08-31 12:55:04 -0500

    Solved excersices

--- rgb_color_gen.py
+++ rgb_color_gen.py
@@ -9,3 +9,7 @@ def rgb_color_gen():
     random_id = random.choices(list, k=3)
     return(random_id)
 print(rgb_color_gen())
+
+numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+positive_even_numbers = [i for i in numbers if i % 2 == 0 and i > 0]
+print(positive_even_numbers)
